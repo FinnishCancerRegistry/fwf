981f62aa752bc19185e4bf9b6f82cdbbeabf874a
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-03-31 12:44:24 +0300

    doc: add v1.0.1 news

--- R/package_fwf.R
+++ R/package_fwf.R
@@ -24,3 +24,7 @@ NULL
 # @codedoc_comment_block news("fwf", "2023-03-31", "1.0.0")
 # Bump to version 1.0.0.
 # @codedoc_comment_block news("fwf", "2023-03-31", "1.0.0")
+
+# @codedoc_comment_block news("fwf", "2023-03-31", "1.0.1")
+# Improve docpage ?fwf::fwf.
+# @codedoc_comment_block news("fwf", "2023-03-31", "1.0.1")
