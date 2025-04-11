df7368da2f2d74841dabd27bece8074cb82cac48
Joonas Miettinen <joonas.miettinen@cancer.fi>
2025-04-11 10:25:36 +0300

    docs: rm @docType

--- R/package_fwf.R
+++ R/package_fwf.R
@@ -1,11 +1,9 @@
-#' @name fwf
-#' @docType package
-#' @title fwf: Fixed Width Format Tables
 #' @eval c(
 #'   codedoc::codedoc_R_package_description("fwf"),
 #'   codedoc::codedoc_news_for_R_package()
 #' )
-NULL
+#' @keywords internal
+"_PACKAGE"
 
 # @codedoc_comment_block R_package_description(fwf)
 # `fwf` is a tiny package to read and write fixed width format tables.
